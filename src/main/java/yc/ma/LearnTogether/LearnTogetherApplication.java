7ad6b0a13a9310e823bab120abd6e639926bc952
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2025-02-03 17:30:45 +0100

    liquibase configured LEAR-44

--- src/main/java/yc/ma/LearnTogether/LearnTogetherApplication.java
+++ src/main/java/yc/ma/LearnTogether/LearnTogetherApplication.java
@@ -2,8 +2,10 @@ package yc.ma.LearnTogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 public class LearnTogetherApplication {
 
 	public static void main(String[] args) {
