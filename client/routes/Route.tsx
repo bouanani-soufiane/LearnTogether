5d4f5ef8fc343380af8559a94d66013a7d7df9fe
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2025-03-02 14:24:10 +0000

    set up typescript config

--- client/routes/Route.tsx
+++ client/routes/Route.tsx
@@ -1,10 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouteObject } from "react-router-dom";
 import Home from "../src/pages/home/Home";
 import Login from "../src/pages/auth/Login";
 import App from "../src/App";
 import AboutUs from "../src/pages/about/AboutUs";
 import Register from "../src/pages/auth/Register";
-const router  = createBrowserRouter([
+
+const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register />
@@ -15,17 +16,18 @@ const router  = createBrowserRouter([
     },
     {
         path: "",
-        element : <App />,
-        children : [
+        element: <App />,
+        children: [
             {
-                path : "/",
-                element : <Home />
+                path: "/",
+                element: <Home />
             },
             {
-                path : "/about",
-                element : <AboutUs />
+                path: "/about",
+                element: <AboutUs />
             }
         ]
     }
-]);
+] as RouteObject[]);
+
 export default router;
